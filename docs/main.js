9f24b293bf7c4a2c7a4f35ad1b927db4f57e4743
g52t <472497084@qq.com>
2022-02-25 00:49:12 +0800

    edit

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,35 @@
+function launchFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    } else if (element.oRequestFullscreen) {
+        element.oRequestFullscreen();
+    }
+}
+
+function stopVideo(el) {
+    $.each($("video"), function (i, v) {
+        if (el !== v)
+            v.pause()
+    });
+}
+
 $(function () {
     $("img.lazyload").lazyload({effect: "fadeIn"});
+    $.each($(".video"), function (i, v) {
+        $(v).find('.btn-play').click(function () {
+            let el = $(v).find('video')[0];
+            stopVideo(el);
+            if (el.paused) {
+                el.play();
+                $(el).attr('controls', 'controls');
+                //launchFullscreen(el);
+            } else
+                el.pause();
+        });
+    })
+
 });
--- docs/publish.py
+++ docs/publish.py
@@ -17,14 +17,24 @@ def get_media_html(path, pre=''):
     path = os.path.abspath(path)
     os.path.join(os.path.dirname(__file__), '..', 'README.md')
     eles = []
+    elesp = []
+    oths = []
     for r in sorted(list(os.listdir(path))):
+        if r[0] == '.':
+            continue
+        if r.endswith('.md'):
+            continue
         fpath = os.path.join(path, r)
         rpath = pre + fpath.replace(path, '')
         # fpath = os.path.abspath(fpath)
         if rpath.endswith('jpeg') or rpath.endswith('png') or rpath.endswith('jpg'):
-            print(path, fpath, rpath)
-            eles.append('<div class="element-item"><img class="image lazyload" data-original="%s"/><span>%s</span></div>' % (rpath, r.split('.')[0].split('-')[-1]))
-    return '\n'.join(eles)
+            # print(path, fpath, rpath)
+            eles.append('<div class="element-item image"><img class="lazyload" data-original="%s"/><span>%s</span></div>' % (rpath, r.split('.')[0].split('-')[-1]))
+        elif rpath.endswith('mp4'):
+            elesp.append('<div class="element-item video"><video src="%s"></video><span>%s<em class="btn-play">播放视频</em></span></div>' % (rpath, r.split('.')[0].split('-')[-1]))
+        else:
+            oths.append('<a target="_blank" href="%s">《%s》</a>' % (rpath, r.split('-')[-1]))
+    return '\n'.join(eles + elesp + oths)
 
 
 xchtml = get_media_html(os.path.join(os.path.dirname(__file__), '..', '相关证据', '宣传'), '相关证据/宣传')
@@ -33,11 +43,20 @@ outt = outt.replace('<!-- xc -->', xchtml)
 sjhtml = get_media_html(os.path.join(os.path.dirname(__file__), '..', '相关证据', '实景'), '相关证据/实景')
 outt = outt.replace('<!-- sj -->', sjhtml)
 
-outpath = os.path.join(os.path.dirname(__file__), '..', 'index.html')
-with io.open(outpath, mode="w", encoding="utf-8") as f:
-    f.write(outt)
+zchtml = get_media_html(os.path.join(os.path.dirname(__file__), '..', '相关证据', '法规政策'), '相关证据/法规政策')
+outt = outt.replace('<!-- zc -->', zchtml)
+
+dfhtml = get_media_html(os.path.join(os.path.dirname(__file__), '..', '相关证据', '答复文件'), '相关证据/答复文件')
+outt = outt.replace('<!-- df -->', dfhtml)
+
+wdhtml = get_media_html(os.path.join(os.path.dirname(__file__), '..', '相关文档'), '相关文档')
+outt = outt.replace('<!-- wd -->', wdhtml)
 
 uptime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 outt = outt.replace('<!-- uptime -->', uptime)
 
-print(datetime.datetime.now())
+outpath = os.path.join(os.path.dirname(__file__), '..', 'index.html')
+with io.open(outpath, mode="w", encoding="utf-8") as f:
+    f.write(outt)
+
+print(uptime)
